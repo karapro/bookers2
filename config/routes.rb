c55b55f24d19fa3a00baabd07e452ad4d1fa85b6
karapro <m01190872c@edu.teu.ac.jp>
2021-07-22 08:55:55 +0000

    本登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
