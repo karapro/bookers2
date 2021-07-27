e5ff73438d91dbfd96b6dbd3c834d0ec3806eb7e
karapro <m01190872c@edu.teu.ac.jp>
2021-07-27 05:52:23 +0000

    bookers2完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {   registrations: 'users/registrations',
+                                      sessions: 'users/sessions' }
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_081354) do
+ActiveRecord::Schema.define(version: 2021_07_24_080330) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
-    t.text "opinion"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2021_07_22_081354) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
