08a76a3283246772cb797b8cb4669c4206688c67
karapro <m01190872c@edu.teu.ac.jp>
2021-08-05 08:23:44 +0000

    最終調整

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:index, :show, :edit, :update]
-  
+
 end
